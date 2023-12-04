809e1eec33cbb50b71a4f51ad8be76d47abb56da
AskarjonAnvarov <127713846+AskarjonAnvarov@users.noreply.github.com>
2023-12-04 20:05:50 +0500

    Create task_2.cpp

---
+++ src/task_2.cpp
@@ -0,0 +1,54 @@
+/*
+ * Author: Asqarjon Anvarov
+ * Date: 04.12.2023
+ * Name: Task 2
+ */
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int N;
+        cin >> N;
+
+        vector<int> A(N);
+        for (int i = 0; i < N; ++i) {
+            cin >> A[i];
+        }
+
+        for (int k = 0; k < N - 2; ++k) {
+            vector<int> medians;
+
+            for (int i = 0; i <= N - 3; i += 2) {
+                vector<int> subarray = {A[i], A[i + 1], A[i + 2]};
+                sort(subarray.begin(), subarray.end());
+                medians.push_back(subarray[1]);
+            }
+
+            int minMedian = *min_element(medians.begin(), medians.end());
+
+           
+            auto it = find(A.begin(), A.end(), minMedian);
+            if (it != A.end()) {
+                A.erase(it);
+            }
+        }
+
+      
+        int sumOfRemainingNumbers = 0;
+        for (int num : A) {
+            sumOfRemainingNumbers += num;
+        }
+
+        cout << sumOfRemainingNumbers << endl;
+    }
+
+    return 0;
+}
